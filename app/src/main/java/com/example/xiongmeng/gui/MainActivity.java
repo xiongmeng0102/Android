046d3256b551bc6b72a18e07764f3842afe1c7df
xiongmeng <229646365@qq.com>
2015-12-02 17:36:16 +0800

    revolve

---
+++ app/src/main/java/com/example/xiongmeng/gui/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.xiongmeng.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    private ImageView img;
+    private Button btnStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img=(ImageView)findViewById(R.id.imgSample);
+        RotateAnimation anime=new RotateAnimation(0,360,0,0);
+        anime.setInterpolator(new LinearInterpolator());
+        anime.setRepeatCount(Animation.INFINITE);
+        img.startAnimation(anime);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
